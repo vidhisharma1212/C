578f614e1274d9a913cccf1298f1687a92143c76
Vidhi Sharma <vidhisharma921@gmail.com>
2021-09-10 22:03:30 +0530

    Switch Case

---
+++ ch_3_conditionals/quiz_question.c
@@ -0,0 +1,29 @@
+#include<stdio.h> 
+
+int main(){
+    int score;
+    printf("Enter your score (1-100) \n");
+    scanf("%d", &score); 
+
+    // switch(score){
+    //     case (score>60 && score<=70 ):
+    //     printf("Your grade is : D");
+    //     break;
+    //     case (score>70 && score<=80 ):
+    //     printf("Your grade is : C");
+    //     break;
+    //     case (score>80 && score<=90 ):
+    //     printf("Your grade is : B");
+    //     break;
+    //     case (score>90 && score<=100 ):
+    //     printf("Your grade is : A");
+    //     break;
+    //     default:
+    //     printf("Your grade is invalid");
+    //     break;
+    // }
+
+    // Doubt
+
+    return 0;
+}
