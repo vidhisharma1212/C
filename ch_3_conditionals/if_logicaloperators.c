8183a1c8a68a20e1fbf4e2e85c6ba1bb4ad7cd16
Vidhi Sharma <vidhisharma921@gmail.com>
2021-09-10 18:05:28 +0530

    If- else conditionals

--- ch_3_conditionals/if_logicaloperators.c
+++ ch_3_conditionals/if_logicaloperators.c
@@ -5,13 +5,25 @@ int main(){
     printf("Enter your age\n");
     scanf("%d", &age);
 
-    if(age<=70 && age>=18){
-        printf("You are above 18 and below 70, so can drive \n");
+    /*
+    && - for and
+    ||- for or
+    !- for not (opposite)
+    eg. !(3==3) will give false
+    !(4==5) will give true
+     */
+
+    int vipPass= 0;
+    // vipPass= 1;
+    // if((age<=70 && age>=18) || !(vipPass==1)) --> then you are allowed at any age..
+
+    if((age<=70 && age>=18) || (vipPass==1)){
+        printf("You are above 18 and below 70, so allowed \n");
         
     }
 
     else{
-        printf("You can not drive.. \n");
+        printf("You are not allowed.. \n");
     }
 
     return 0;
