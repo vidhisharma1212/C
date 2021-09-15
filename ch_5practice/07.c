de9b6f60a97fbd9e19bb619a6179992d4b93c83b
Vidhi Sharma <vidhisharma921@gmail.com>
2021-09-15 21:56:26 +0530

    Functions and recursions practice

--- ch_5practice/07.c
+++ ch_5practice/07.c
@@ -5,6 +5,7 @@ int main(){
     int i, n;
     printf("Enter a number here: \n");
     scanf("%d", &n);
+
     star(n);
 
     // for(i=1 ;i<=n ; i+=2){
@@ -14,6 +15,19 @@ int main(){
     return 0;
 }
 
+// formula-
+// 2n-1
+
 void star(int n){
-    printf("*");
+    if (n==1){
+        printf("*\n");
+        return;
+    }
+    star(n-1);
+    int i;
+    for(i=0; i<(2*n-1); i++){
+        printf("*");
+        }
+        
+    printf("\n");
 }
