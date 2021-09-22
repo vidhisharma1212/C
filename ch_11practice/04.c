72d27ec1610f59f5862fa88061e09601af01f534
Vidhi Sharma <vidhisharma921@gmail.com>
2021-09-23 00:40:41 +0530

    Dynamic Memory allocation Practice

---
+++ ch_11practice/04.c
@@ -0,0 +1,26 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+
+int main(){
+    int *ptr;
+    ptr= (int *) malloc(5 * sizeof(int));
+    for (int i=0; i<5; i++){
+        printf("Enter the %d th value\n", i+1);
+        scanf("%d", &ptr[i]);
+    }
+
+    for (int i=0; i<5; i++){
+        printf("The value of %d th element is %d \n", i+1, ptr[i]);
+    }
+
+    ptr= realloc(ptr, 10*sizeof(int));
+    for (int i=0; i<10; i++){
+        printf("Enter the %d th value\n", i+1);
+        scanf("%d", &ptr[i]);
+    }
+
+    for (int i=0; i<10; i++){
+        printf("The value of %d th element is %d \n", i+1, ptr[i]);
+    }
+    return 0;
+}
---
+++ ch_11practice/05.c
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+
+int main(){
+    int *ptr;
+    ptr= (int *) malloc(10 * sizeof(int));
+    for (int i=0; i<10; i++){
+        printf("7 X %d = %d\n", i+1, (7*(i+1)));
+    }
+        printf("**********Reallocating***********\n");
+
+    ptr= realloc(ptr, 10* sizeof(int));
+    for (int i=0; i<15; i++){
+        printf("7 X %d = %d\n", i+1, (7*(i+1)));
+    }
+    return 0;
+}
